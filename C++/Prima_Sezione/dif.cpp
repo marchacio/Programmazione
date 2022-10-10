16f53dc56f598310f26988eba2bd1d5be609ecec
Marco Zoratti <marco.zoratti.03@gmail.com>
2022-10-10 18:05:08 +0200

    Es 1.4

---
+++ C++/Prima_Sezione/dif.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //Dichiaro due variabili
+  int a, b;
+
+  //Assegno dei valori di input ad esse
+  cout << "Inserisci il valore intero che vuoi dare al primo numero: ";
+  cin >> a;
+
+  cout << "Inserisci il valore intero che vuoi dare al secondo numero: ";
+  cin >> b;
+
+  cout << "La differenza vale " << a - b << endl;
+  return 0;
+}
+
+// chiedere di inserire due valori interi
+// dichiarare due variabili a e b di tipo int
+// leggere a e b
+// stampare la stringa "La differenza vale "
+// stampare il valore di a - b
+// stampare una andata a capo
