1391d3fe4db5192e097ab22a7fee108885791203
Marco Zoratti <marco.zoratti.03@gmail.com>
2023-01-09 11:27:04 +0100

    EXTRA

---
+++ ESAMI/Giugno 2022/Parte1/Cognome1.cpp
@@ -0,0 +1,17 @@
+#include "hist.h"
+
+
+// Aggiunge
+void add(Hist & h, const int v)
+{
+}
+
+// ordina
+void sort(Hist & h)
+{
+}
+
+// Cerca, restituisce conteggio
+int count(const Hist & h, int v)
+{
+}
---
+++ ESAMI/Giugno 2022/Parte2/Cognome2.cpp
@@ -0,0 +1,13 @@
+#include "ll.h"
+
+void addhead(ll &s, char & c)
+{
+}
+
+bool removehead(ll &s, char & c)
+{
+}
+
+int size(ll s)
+{
+}
